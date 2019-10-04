8fd62baa5678fbaa0c5e542e848d60462fd3c930
Tom SOLVERY <solverytom@hotmail.fr>
2019-10-04 10:23:58 +0200

    Auto stash before merge of "master" and "origin/master"

--- rangeSlider/RangeSlider.java
+++ rangeSlider/RangeSlider.java
@@ -9,13 +9,25 @@ public class RangeSlider {
 
 	Curseur [] ct;
 
+	private int curs_inf;
+	private int curs_sup;
+
 	public RangeSlider() {
 		Curseur curs_inf = new Curseur(100, 100, 30, 30);
 		Curseur curs_sup = new Curseur(600, 100, 30, 30);
 		ct = new Curseur [] {curs_inf, curs_sup};
 	}
 
-	void setc(Curseur c, int v) {
-		
+	public void setCurseur(Curseur c, int v) throws Exception {
+		switch(c) {
+		case Inferieur : 
+			curs_inf = v;
+			break;
+		case Superieur : 
+			curs_sup = v;
+			break;
+		default : 
+			throw new Exception("Erreur Curseur Inconnu");
+		}
 	}
 }
