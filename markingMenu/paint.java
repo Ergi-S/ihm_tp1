b22b90bab33985e8ced75386c840e8c42eef7fb3
Tom SOLVERY <solverytom@hotmail.fr>
2019-10-18 15:13:45 +0200

    Selection des couleurs

--- markingMenu/paint.java
+++ markingMenu/paint.java
@@ -1,15 +1,16 @@
 package markingMenu;
 
 //////////////////////////////////////////////////////////////////////////////
+
 // file    : Paint.java
 // content : basic painting app
 //////////////////////////////////////////////////////////////////////////////
-
 
 /* imports *****************************************************************/
 
 import static java.lang.Math.*;
 
+import java.util.Hashtable;
 import java.util.Vector;
 
 import java.awt.BorderLayout;
@@ -33,17 +34,20 @@ import javax.swing.AbstractAction;
 import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 
-
 /* paint *******************************************************************/
 
 class Paint extends JFrame {
 	Vector<Shape> shapes = new Vector<Shape>();
+	Hashtable<Shape, Color> colorShape = new Hashtable<Shape, Color>();
 
-	class Tool extends AbstractAction
-	           implements MouseInputListener {
-	   Point o;
+	class Tool extends AbstractAction implements MouseInputListener {
+		Point o;
 		Shape shape;
-		public Tool(String name) { super(name); }
+
+		public Tool(String name) {
+			super(name);
+		}
+
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("using tool " + this);
 			panel.removeMouseListener(tool);
@@ -52,78 +56,130 @@ class Paint extends JFrame {
 			panel.addMouseListener(tool);
 			panel.addMouseMotionListener(tool);
 		}
-		public void mouseClicked(MouseEvent e) {}
-		public void mouseEntered(MouseEvent e) {}
-		public void mouseExited(MouseEvent e) {}
-		public void mousePressed(MouseEvent e) { o = e.getPoint(); }
-		public void mouseReleased(MouseEvent e) { shape = null; }
-		public void mouseDragged(MouseEvent e) {}
-		public void mouseMoved(MouseEvent e) {}
-	}
-	
-	Tool tools[] = {
-		new Tool("pen") {
-			public void mouseDragged(MouseEvent e) {
-				Path2D.Double path = (Path2D.Double)shape;
-				if(path == null) {
-					path = new Path2D.Double();
-					path.moveTo(o.getX(), o.getY());
-					shapes.add(shape = path);
-				}
-				path.lineTo(e.getX(), e.getY());
-				panel.repaint();
-			}
-		},
-		new Tool("rect") {
-			public void mouseDragged(MouseEvent e) {
-				Rectangle2D.Double rect = (Rectangle2D.Double)shape;
-				if(rect == null) {
-					rect = new Rectangle2D.Double(o.getX(), o.getY(), 0, 0);
-					shapes.add(shape = rect);
-				}
-				rect.setRect(min(e.getX(), o.getX()), min(e.getY(), o.getY()),
-				             abs(e.getX()- o.getX()), abs(e.getY()- o.getY()));
-				panel.repaint();
-			}
-		},
-		new Tool("ellipse") {
-			public void mouseDragged(MouseEvent e) {
-				Ellipse2D.Double ellipse = (Ellipse2D.Double)shape;
-				if(ellipse == null) {
-					ellipse = new Ellipse2D.Double(o.getX(),o.getY(),0,0);
-					shapes.add(shape = ellipse);
-				}
-				ellipse.setFrame(min(e.getX(), o.getX()), min(e.getY(), o.getY()),
-				             abs(e.getX()- o.getX()), abs(e.getY()- o.getY()));
-				panel.repaint();
-			}
+
+		public void mouseClicked(MouseEvent e) {
 		}
-	};
+
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		public void mouseExited(MouseEvent e) {
+		}
+
+		public void mousePressed(MouseEvent e) {
+			o = e.getPoint();
+		}
+
+		public void mouseReleased(MouseEvent e) {
+			shape = null;
+		}
+
+		public void mouseDragged(MouseEvent e) {
+		}
+
+		public void mouseMoved(MouseEvent e) {
+		}
+	}
+
+	Tool tools[] = { new Tool("pen") {
+		public void mouseDragged(MouseEvent e) {
+			Path2D.Double path = (Path2D.Double) shape;
+			if (path == null) {
+				path = new Path2D.Double();
+				path.moveTo(o.getX(), o.getY());
+				shapes.add(shape = path);
+
+			}
+			path.lineTo(e.getX(), e.getY());
+			colorShape.put(path,selectedColor);
+			panel.repaint();
+		}
+	}, new Tool("rect") {
+		public void mouseDragged(MouseEvent e) {
+			Rectangle2D.Double rect = (Rectangle2D.Double) shape;
+			if (rect == null) {
+				rect = new Rectangle2D.Double(o.getX(), o.getY(), 0, 0);
+				shapes.add(shape = rect);
+
+			}
+
+			rect.setRect(min(e.getX(), o.getX()), min(e.getY(), o.getY()), abs(e.getX() - o.getX()),
+					abs(e.getY() - o.getY()));
+			colorShape.put(rect,selectedColor);
+			panel.repaint();
+		}
+	}, new Tool("ellipse") {
+		public void mouseDragged(MouseEvent e) {
+			Ellipse2D.Double ellipse = (Ellipse2D.Double) shape;
+			if (ellipse == null) {
+				ellipse = new Ellipse2D.Double(o.getX(), o.getY(), 0, 0);
+				shapes.add(shape = ellipse);
+
+			}
+
+			ellipse.setFrame(min(e.getX(), o.getX()), min(e.getY(), o.getY()), abs(e.getX() - o.getX()),
+					abs(e.getY() - o.getY()));
+			colorShape.put(ellipse,selectedColor);
+			panel.repaint();
+		}
+	} };
+
+	class Coloration extends AbstractAction {
+		Color color;
+
+		public Coloration(Color c,String name) {
+			super(name);
+			this.color = c;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			/*if (!shapes.isEmpty()) {
+				colorShape.put(shapes.lastElement(), this.color);
+			}*/
+			selectedColor = this.color;
+		}
+	}
+
 	Tool tool;
+
+	Coloration[] colors = { new Coloration(Color.BLACK,"Noir"), new Coloration(Color.BLUE,"Bleu"), new Coloration(Color.RED,"Rouge"),
+			new Coloration(Color.GREEN,"Vert"), new Coloration(Color.YELLOW,"Jaune") };
 
 	JPanel panel;
 	
+	Color selectedColor = Color.BLACK;
+
 	public Paint(String title) {
 		super(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setMinimumSize(new Dimension(800, 600));
-		add(new JToolBar() {{
-			for(AbstractAction tool: tools) {
-				add(tool);
+		add(new JToolBar() {
+			{
+				for (AbstractAction tool : tools) {
+					add(tool);
+				}
+				for (AbstractAction c : colors) {
+					add(c);
+				}
+
 			}
-		}}, BorderLayout.NORTH);
-		add(panel = new JPanel() {	
+		}, BorderLayout.NORTH);
+		add(panel = new JPanel() {
 			public void paintComponent(Graphics g) {
-				super.paintComponent(g);	
-				Graphics2D g2 = (Graphics2D)g;
-				g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
-				                    RenderingHints.VALUE_ANTIALIAS_ON);
-		
+				super.paintComponent(g);
+				Graphics2D g2 = (Graphics2D) g;
+				g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
 				g2.setColor(Color.WHITE);
 				g2.fillRect(0, 0, getWidth(), getHeight());
-				
-				g2.setColor(Color.BLACK);
-				for(Shape shape: shapes) {
+
+				for (Shape shape : shapes) {
+					Color c = colorShape.get(shape);
+					System.out.println(c);
+					/*if (c == null)
+						c = Color.BLACK;*/
+					g2.setColor(c);
 					g2.draw(shape);
 				}
 			}
@@ -133,8 +189,7 @@ class Paint extends JFrame {
 		setVisible(true);
 	}
 
-
-/* main *********************************************************************/
+	/* main *********************************************************************/
 
 	public static void main(String argv[]) {
 		SwingUtilities.invokeLater(new Runnable() {
